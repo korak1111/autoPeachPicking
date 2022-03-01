d975843bb3454109235cc629349777f8f0825cce
korak1111 <71841158+korak1111@users.noreply.github.com>
2022-03-01 15:28:40 -0500

    Full peach harvest and collection method with real locations. Add pixel to coordinate calculations (wip). Add camera capture for rgb and depth images.

--- Matlab/vision_test.m
+++ Matlab/vision_test.m
@@ -1,54 +1,43 @@
 % Run this once in the command window
-% imaqregister('<full/path/to/kinova_vision_imaq library>');
+% imaqregister('C:\Program Files\Kinova\Vision Imaq\kinova_vision_imaq.dll');
 
-% --- Display the Color Device Video --- %
-vid1 = videoinput('kinova_vision_imaq', 1, 'RGB24');
-vid1.FramesPerTrigger = 1;
-src1 = getselectedsource(vid1);
-
-% Optionally, view the adaptor version
-imaqhwinfo(vid1)
-
-% Optionally, change device properties
-src1.CloseConnectionOnStop = 'Enabled';
-src1.Ipv4Address = '10.20.0.100';
-src1.ResetROIOnResolutionChange = 'Disabled';
-
-% Optionally, change the Region of Interest
-vid1.ROIPosition = [0 0 300 200];
-
-preview(vid1);
-closepreview(vid1);
-delete(vid1);
-
-% --- Display the Depth Device Raw Video --- %
-% Note: For Linux, the adaptor name is libkinova_vision_imaq
-vid2 = videoinput('kinova_vision_imaq', 2, 'MONO16');
-vid2.FramesPerTrigger = 1;
-src2 = getselectedsource(vid2);
-
-% Optionally, view the adaptor version
-imaqhwinfo(vid2)
-
-% Optionally, change device properties
-src2.CloseConnectionOnStop = 'Enabled';
-src2.Ipv4Address = '10.20.0.100';
-src2.ResetROIOnResolutionChange = 'Disabled';
-
-% Optionally, change the Region of Interest
-vid2.ROIPosition = [0 0 300 200];
-
-preview(vid2);
-closepreview(vid2);
-delete(vid2);
-
-% --- Settings --- %
-% SetOptionValue
-% OPTION_DEPTH_UNITS	Number of meters represented by a single depth unit 
-% (supported on depth sensor only: 0.0001 to 0.0100, step 0.000001)
-
-% depthData = getsnapshot(vidDepth);
-% rgbData = getsnapshot(vidRGB);
-
-% imwrite(A,'myGray.png')
-
+function capture_rgb_and_depth_img
+    imaqreset;
+    imaqhwinfo;
+    
+    % --- Display the Color Device Video --- %
+    vid1 = videoinput('kinova_vision_imaq', 1, 'RGB24');
+    vid1.FramesPerTrigger = 1;
+    src1 = getselectedsource(vid1);
+    
+    % Change device properties
+    src1.CloseConnectionOnStop = 'Enabled';
+    src1.ResetROIOnResolutionChange = 'Disabled';
+    
+    preview(vid1);
+    rgbData = getsnapshot(vid1);
+    imwrite(rgbData, 'rbg_img.png')
+    
+    closepreview(vid1);
+    delete(vid1);
+    
+    % --- Display the Depth Device Raw Video --- %
+    vid2 = videoinput('kinova_vision_imaq', 2, 'MONO16');
+    vid2.FramesPerTrigger = 1;
+    src2 = getselectedsource(vid2);
+    
+    % Change device properties
+    src2.CloseConnectionOnStop = 'Enabled';
+    src2.ResetROIOnResolutionChange = 'Disabled';
+    
+    preview(vid2);
+    depthData = getsnapshot(vid2);
+    imwrite(depthData, 'depth_img.png')
+    
+    % Depth units in mm
+    
+    depthData(135, 240)
+    
+    closepreview(vid2);
+    delete(vid2);
+end
