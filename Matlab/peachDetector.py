ea9c8632ff48df3aa7278c537dcbf4199a1f0e2a
juliamco <59571043+juliamco@users.noreply.github.com>
2022-03-02 15:52:38 -0500

    Saving predicted image for testing!

--- Matlab/peachDetector.py
+++ Matlab/peachDetector.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import cv2
 from PIL import Image
-from time import time
+import time
 import os.path
 from os import path
 
@@ -38,41 +38,68 @@ class PeachDetector:
     labels, coord = results
     n = len(labels)
     x_shape, y_shape = frame.shape[1], frame.shape[0]
+    coordinates = []
     for i in range(n):
       row = coord[i]
       if row[4] >= 0.2: ## Thresholding
         x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+        coordinates.append([x1, y1, x2, y2])
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2, cv2.LINE_AA)
+
+    return frame, coordinates
+
+  def plot_boxes_original_image(self, frame, coords):
+    for x1, y1, x2, y2 in coords:
+      cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
     return frame
 
-  def predict(self):
-    frame = cv2.imread('test_peach_img.jpeg')
+
+  def predict(self, image):
+    frame = cv2.imread(image)
+    original_height, original_width = frame.shape[0], frame.shape[1]
+
     frame = cv2.resize(frame, (416, 416))
 
-    start_time = time()
     results = self.detect_peaches(frame)
-    frame = self.plot_boxes(results, frame)
-    end_time = time()
-    # fps = 1/np.round(end_time - start_time, 2)
-    # print(f'Frames Per Second : {fps}')
+    frame, coordinates = self.plot_boxes(results, frame)
 
-    # cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
-    # cv2.imshow('YOLOv5 Detection', frame)
+    original_predicted_image = self.plot_boxes_original_image(frame, coordinates)
+    cv2.imwrite('predicted_image.png', original_predicted_image)
 
-    # cv2.namedWindow('imageWindow')
-    # cv2.imshow('imageWindow', frame)
+    translated_coordinates = []
 
-    # wait = True
-    # while wait:
-    #   if cv2.waitKey(5) & 0xFF == 27:
-    #     break
+    for x1, y1, x2, y2 in coordinates:
+      x1_translated = (original_width/416) * x1
+      y1_translated = (original_height/416) * y1
+      x2_translated = (original_width/416) * x2
+      y2_translated = (original_height/416) * y2
 
-# def predict():
-#   detector = PeachDetector(capture_index=1, model_name='best.pt')
-#   detector()
+      center_x = (x1_translated + x2_translated)/2
+      center_y = (y1_translated + y2_translated)/2
+      translated_coordinates.append([center_x, center_y])
 
-# while 1:
-#   while 
+    return translated_coordinates #resized to proper res
 
-path.exists('test_peach_img.jpeg'
+# Initialize NN 
+
+detector = PeachDetector(capture_index=1, model_name='best.pt')
+
+file_name='rgb_img.png'
+
+#Master Script Loop
+while 1:
+  no_picture = True
+  while no_picture: # Waiting for a picutre to be delivered
+    no_picture = not path.exists(file_name) # check if a file is there
+    print("No Picture Found")
+
+  time.sleep(0.5)
+  coords = detector.predict(file_name)  # Get Coordinates of Bounding Box
+  os.remove(file_name) # Delete Picture
+
+  with open('coords.txt', 'x') as f: #Write to a file and release control of file
+    for x, y in coords:
+      line = str(x) + ' ' + str(y)
+      f.write(str(line))
+      f.write('\n')
