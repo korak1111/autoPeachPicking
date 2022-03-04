e42bd297ddf0b87a9ff5fb88141045efd66d97ef
egdavis1 <35307849+egdavis1@users.noreply.github.com>
2022-03-04 09:04:45 -0500

    Changed Collection to Joint Angles from Cartesian

--- Matlab/constants.m
+++ Matlab/constants.m
@@ -7,7 +7,10 @@ classdef constants
         % Arm in starting position to begin looking for peaches
         HOME_POSITION = [2.6 46.43 356.22 214.97 162.74 347.51 282.18];
         % Peach collection area position (0, 0)
-        COLLECTION_POSITION = [0.11 11.34 180.48 246.48 185.44 50.35 84.22];
+        COLLECTION_POSITION_1 = [0.11 11.34 180.48 246.48 185.44 50.35 84.22];
+        COLLECTION_POSITION_2 =[341.01 359.23 175.47 230.44 186.29 45.98 76.15];
+        COLLECTION_POSITION_3 =[345.15 12.73 178.8 246.49 185.82 47.79 84.19];
+        COLLECTION_POSITION_4 =[0.56 358.95 180.16 229.56 185.93 46.22 83.55];
         % Gripper Commands
         OPEN_GRIPPER = 0;
         CLOSE_GRIPPER = 1;
